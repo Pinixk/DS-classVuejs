ae4adf47e1ad59ca04e06400d7bc81f390e76cde
Pinixk <bjkim0110@gmail.com>
2022-06-07 17:42:23 +0900

    06/07

---
+++ workspace/basic-vue/src/components/Vue26_component_create.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1>26 component create</h1>
+  <div id="app">
+    <p>{{ title }}</p>
+    <counter></counter>
+    <counter></counter>
+    <counter></counter>
+  </div>
+</template>
+
+<script>
+import { createApp, ref } from "@vue/runtime-dom";
+const app = createApp({});
+app.component("counter", {
+  inject: ["counter_header"],
+  setup() {
+    const count = ref(0);
+    return { count };
+  },
+  template: [
+    "<span>{{counter_header}}</span>",
+    '<button @click="counter++" v-text="count"></button>',
+    "<hr>",
+  ],
+});
+app.config.globalProperties.title = "Vue 3 Demo";
+app.provide("counter_header", "counter");
+app.mount("#app");
+export default {};
+</script>
