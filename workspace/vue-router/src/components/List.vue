3e7e9e762700fefe606d697439491c20ffb5ab39
Pinixk <bjkim0110@gmail.com>
2022-06-10 14:08:41 +0900

    06/10

--- workspace/vue-router/src/components/List.vue
+++ workspace/vue-router/src/components/List.vue
@@ -1,53 +1,60 @@
 <template>
-  <div style="width:100%; height:100%; display:flex;">
-    <div class="aboutCon" style="margin:auto;">
-        <div class="header">
-            <h1>회원 정보</h1>
-        </div>
-        <div>
-            <div v-for="(item,idx) in header" :key="idx">{{item}}</div>
-            <table>
-                <thead>
-                    <th v-for="(item,idx) in header" :key="idx">{{item}}</th>
-                </thead>
-                <tbody>
-                    <tr>
-                      
-
-                    </tr>
-                </tbody>
-         
-            </table>
-        </div>
+  <div class="list">
+    <div class="aboutCon">
+      <div class="listH">
+        <h1>회원 정보</h1>
+      </div>
+      <table>
+        <thead>
+          <th v-for="(item, idx) in listT" :key="idx">{{ item }}</th>
+        </thead>
+        <tbody>
+          <tr v-for="(item, idx) in users" :key="idx">
+            <td>{{ item.id }}</td>
+            <td>{{ item.name }}</td>
+            <td>{{ item.gender }}</td>
+            <td>{{ item.number }}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
+import userList from "../assets/data/user.json";
+
 export default {
-  name: "ToAbout",
-  set(){
-    const header = ['순번','이름','ID','전화번호','E-mail'];
-    const menber = [
-        ['1st','2nd','3rd','4th'],
-        ['짱구','철수','훈이','유리'],
-        ['Zzang9','stealWater','ugly2','glassy'],
-        ['010-0000-0000','010-1111-1111','010-0101-1010','010-xxxx-xxxx'],
-        ['Zzang9@gmail.com', 'stealWater@naver.com', 'ugly2@nate.com', 'glassy@hanmail.net'],
-    ];
-
-
-    return {header, menber}
+  name: "ToList",
+  setup() {
+    const listT = ["ID", "이름", "성별", "전화번호"];
+    return {  listT};
+  },
+  computed: {
+    users() { 
+      return userList.users.map((items) => { 
+      return items;
+      })
+    },
   }
-};
+}
 </script>
 
 <style>
-.header{
-    margin: 150px 0px 30px;
+.list {
+  width: 100%;
+  height: 100%;
+  display: flex;
 }
-td,th {
+.aboutCon {
+  margin: auto;
+}
+.listH {
+  margin: 150px 0px 30px;
+}
+td,
+th {
   border-bottom: 1px solid black;
-  padding: 15px;
+  padding: 20px;
 }
 </style>
