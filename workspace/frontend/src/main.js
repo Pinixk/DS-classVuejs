f00e448af96231837f647680a95c684428ca1b50
Pinixk <bjkim0110@gmail.com>
2022-06-17 17:13:35 +0900

    06/17

--- workspace/frontend/src/main.js
+++ workspace/frontend/src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import {router} from './routes/index'
+import axios from 'axios'
 
-createApp(App).mount('#app')
+createApp.config.productionTip = false
+createApp.prototype.$http = axios
+
+const app = createApp({
+   render: h => h(App),
+   router,
+})
+
+app.mount('#app')
